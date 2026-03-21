d6fae828f6086fa0d34b74e1790915669cb89d44
Shrinivas <jshrinivas1996@gmail.com>
2026-03-21 23:15:40 +0530

    Project setup

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <div>
+            Im Footer
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/data/products.ts
@@ -0,0 +1,10 @@
+export const products = [
+  {
+    id: 1,
+    name: "Chest Press Machine",
+    price: "₹85,000",
+    image: "/images/chest-press.jpg",
+    description: "Heavy-duty chest press machine...",
+    category: "Strength"
+  }
+];
