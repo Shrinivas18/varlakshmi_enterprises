7be8bb9143630345a9952308ee76a884117fc297
Shrinivas <jshrinivas1996@gmail.com>
2026-03-25 23:57:34 +0530

    footer enhancement

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,30 @@
-function Footer(){
-    return(
-        <div className="bg-gray-500 h-80 ">
-            <div className="flex row lg:gap-120 lg:pl-12">
-                <div>
-                    Contact
-                </div>
-                <div>
-                    Social
-                </div>
-                <div>
-                    Licensing
-                </div>
-            </div>
+function Footer() {
+  return (
+    <footer className="mt-auto">
+      <div
+        className="h-80 bg-cover bg-center flex flex-col justify-center items-center text-white"
+        style={{
+          backgroundImage: "url('/footer.png')",
+        }}
+      >
+        <h2 className="text-2xl font-semibold mb-6">
+          Stay Connected
+        </h2>
+
+        <div className="flex gap-6">
+          <button className="px-4 py-2 bg-black/50 rounded">
+            Contact
+          </button>
+          <button className="px-4 py-2 bg-black/50 rounded">
+            Social
+          </button>
+          <button className="px-4 py-2 bg-black/50 rounded">
+            Licensing
+          </button>
         </div>
-    )
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
